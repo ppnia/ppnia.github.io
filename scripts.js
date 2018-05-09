0b37f0fa392821b17fb5c2ccbbeac5b54c165043
Pinia Chandra <ppnia@Pinias-MBP.hitronhub.home>
2018-05-08 23:45:41 -0700

    changed the whole website

---
+++ scripts.js
@@ -0,0 +1,59 @@
+//The basic of this pen is from Codewoofy's Eyes Follow mouse. It is changed to suit the oval shape of the eyes. https://codepen.io/Codewoofy/pen/VeBJEP
+
+$(document).ready(function() {
+    
+//moving the eyes
+$("body").mousemove(function(event) {
+  var eye = $(".eye");
+  var pupil = $(".pupil");
+  // var foot = $(".leg");
+  
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  
+  var rotYScaled = rot;
+  if (rot > 180) {
+    rotYScaled = 360-rot;
+  }
+  var scaledY = (rotYScaled/180) * 100 - 50;
+  
+  var rotXScaled = rot;
+  if (rotXScaled >= 270) {
+    rotXScaled = rot - 270;
+  } else {
+    rotXScaled = rot + 90;
+  }
+  if (rotXScaled > 180) {
+    rotXScaled = 360-rotXScaled;
+  }
+  var scaledX = (rotXScaled/180) * 100 - 50;
+  
+  pupil.css({
+    'transform': 'translate(' + scaledX + '%,' + scaledY + '%)'
+  });
+});
+    
+
+//animating the button
+var button = $(".button.main");
+
+$(".collapse").on("show.bs.collapse", function() {
+  button.css({
+    "background-color": "#ba5d6c",
+    "box-shadow": "1em #666",
+    transform: "translateY(0.3em)"
+  });
+});
+
+$(".collapse").on("hide.bs.collapse", function() {
+  button.css({
+    "background-color": "#ed7186",
+    "box-shadow": "0em #666",
+    transform: "translateY(-0.3em)"
+  });
+});
+    
+});
